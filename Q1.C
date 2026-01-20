ddf8cbeeefff487b8e0d1102e9c4540f807ed59a
sahilpawar1111 <sahilpawar2713@gmail.com>
2026-01-20 17:55:03 +0530

    Add files via upload

---
+++ Q1.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str [20];
+	int freq[123] = {0};
+	
+	printf("Enter your str : ");
+	scanf("%s",&str);
+	
+	for(int i=0;i<strlen(str); i++)
+     {
+     	freq[str[i]]++;
+		 }
+		 
+    printf("\n freq is : ");		 	
+	
+	for (int i = 0; i <= 122; i++)
+	{
+		if(freq[i]>0)
+		{
+			printf("\n %c => %d",i,freq[i]);
+		}
+	}
+	
+	return 0;
+}
